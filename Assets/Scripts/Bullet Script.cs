262813eb70ef8f5c50fc4eb378ed91b0dba96163
TopherOverbeyDrac <topher.overbey@gmail.com>
2025-10-28 11:48:20 -0700

    Topher's fist updoot

---
+++ Assets/Scripts/Bullet Script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Topher Overbey
+ * 10/28/2025
+ * Control the power of a Bullet when it collides with a enemy
+ */
+
+public class BulletScript : MonoBehaviour
+{
+    public int bulletDamage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemy Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Topher Overbey
+ * 10/28/2025
+ * Controls the movement and the damage that an enemy can take
+ */
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
